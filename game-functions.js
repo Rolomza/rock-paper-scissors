48b07c87d540b20a1fc9ca037db33cbf00c18789
Augusto <augusto.robles.mza@gmail.com>
2021-03-08 00:26:02 -0300

    Main game function

--- game-functions.js
+++ game-functions.js
@@ -1,13 +1,8 @@
-let playerSelection = prompt('Choose an object: (rock, paper, scissors): ').toLowerCase()
-
-
 const computerPlay = () => {
     let objects = ["rock", "paper", "scissors"]
     let randomObject = objects[Math.round(Math.random() * 2)]
     return randomObject
 }
-
-let computerSelection = computerPlay()
 
 const playRound = (playerSelection, computerSelection) => {
     if(playerSelection === 'rock' && computerSelection === "scissors") {
@@ -31,4 +26,17 @@ const playRound = (playerSelection, computerSelection) => {
     }
 }
 
-console.log(playRound(playerSelection, computerSelection))
+const game = () => {
+    console.log('You have 5 rounds to play! Come on!')
+    let counter = 0
+    while(counter < 5) {
+        let playerSelection = prompt('Choose an object: (rock, paper, scissors): ').toLowerCase()
+        let computerSelection = computerPlay()
+        playRound(playerSelection, computerSelection)
+        console.log(playRound(playerSelection, computerSelection))
+        counter++
+    }
+    console.log('The game has finished.')
+}
+
+game()
