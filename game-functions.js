fe14067c3c7f939a827de58ac12cf04278af3a72
Augusto <augusto.robles.mza@gmail.com>
2021-03-08 00:39:51 -0300

    playRound refactoring

--- game-functions.js
+++ game-functions.js
@@ -5,24 +5,14 @@ const computerPlay = () => {
 }
 
 const playRound = (playerSelection, computerSelection) => {
-    if(playerSelection === 'rock' && computerSelection === "scissors") {
-        return 'You win! Rock beats scissors.'
-    } else if (playerSelection === 'rock' && computerSelection === "paper") {
-        return 'You lose! Paper beats rock.'
-    } else if (playerSelection === 'rock' && computerSelection === "rock") {
+    if((playerSelection === 'rock' && computerSelection === "scissors") || 
+       (playerSelection === 'scissors' && computerSelection === "paper") ||
+       (playerSelection === 'paper' && computerSelection === "rock")) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    } else if (playerSelection === computerSelection) {
         return 'Tie!'
-    } else if (playerSelection === 'scissors' && computerSelection === "rock") {
-        return 'You lose! Rock beats scissors.'
-    } else if (playerSelection === 'scissors' && computerSelection === "paper") {
-        return 'You win! Scissors beats paper.'
-    } else if (playerSelection === 'scissors' && computerSelection === "scissors") {
-        return 'Tie!'
-    } else if (playerSelection === 'paper' && computerSelection === "rock") {
-        return 'You win! Paper beats rock.'
-    } else if (playerSelection === 'paper' && computerSelection === "scissors") {
-        return 'You lose! Scissors beats paper.'
-    } else if (playerSelection === 'paper' && computerSelection === "paper") {
-        return 'Tie!'
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
     }
 }
 
